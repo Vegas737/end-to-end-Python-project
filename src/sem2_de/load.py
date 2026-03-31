d9e4b97a59ade104b16cd16adb97ed2cedc5fe42
Vegas737 <vegasgit737@gmail.com>
2026-03-31 15:51:32 +0400

    Update load.py

--- src/sem2_de/load.py
+++ src/sem2_de/load.py
@@ -6,7 +6,7 @@ from datetime import datetime
 conn_url = "postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres"
 engine = create_engine(conn_url)
 
-mart_dir = "../data/mart/variant_20"
+mart_dir = "data/mart/variant_20"
 
 files = [f for f in os.listdir(mart_dir) if f.startswith("mart_monthly")]
 files.sort()
