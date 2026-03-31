72b46d53cf9eedda6ca8d92c0df99d18bbba0dd8
Vegas737 <vegasgit737@gmail.com>
2026-03-31 15:03:35 +0400

    Create load.py

---
+++ src/sem2_de/load.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from datetime import datetime
+
+conn_url = "postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres"
+engine = create_engine(conn_url)
+
+mart_dir = "../data/mart/variant_20"
+
+files = [f for f in os.listdir(mart_dir) if f.startswith("mart_monthly")]
+files.sort()
+latest_mart = os.path.join(mart_dir, files[-1])
+
+df = pd.read_csv(latest_mart)
+
+with engine.begin() as connection:
+    df.to_sql(
+        name='mart_holidays_germany', 
+        con=connection, 
+        if_exists='replace', 
+        index=False
+    )
+
+print(f"✅ Успешно загружено {len(df)} строк в таблицу 'mart_holidays_germany'")
+print(f"Использован файл: {latest_mart}")
