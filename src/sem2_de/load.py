d1e9be3909a66ada760457df2ff62669f86f516c
Vegas737 <vegasgit737@gmail.com>
2026-04-07 14:22:52 +0400

    Update load.py

--- src/sem2_de/load.py
+++ src/sem2_de/load.py
@@ -1,26 +1,24 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import os
-from datetime import datetime
 
 conn_url = "postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres"
 engine = create_engine(conn_url)
 
-mart_dir = "data/mart/variant_20"
+normalized_file = "data/normalized/holidays_cleaned.csv"
 
-files = [f for f in os.listdir(mart_dir) if f.startswith("mart_monthly")]
-files.sort()
-latest_mart = os.path.join(mart_dir, files[-1])
-
-df = pd.read_csv(latest_mart)
-
-with engine.begin() as connection:
-    df.to_sql(
-        name='mart_holidays_germany', 
-        con=connection, 
-        if_exists='replace', 
-        index=False
-    )
-
-print(f"✅ Успешно загружено {len(df)} строк в таблицу 'mart_holidays_germany'")
-print(f"Использован файл: {latest_mart}")
+if os.path.exists(normalized_file):
+    df = pd.read_csv(normalized_file)
+    print(f"✅ Загружаем данные из слоя Normalized: {normalized_file}")
+    
+    with engine.begin() as connection:
+        df.to_sql(
+            name='mart_holidays_germany', 
+            con=connection, 
+            if_exists='replace',
+            index=False
+        )
+    print(f"✅ Успешно загружено {len(df)} строк в таблицу 'mart_holidays_germany'")
+else:
+    print(f"❌ Ошибка: Файл {normalized_file} не найден. Проверь этап Transform!")
+    exit(1)
