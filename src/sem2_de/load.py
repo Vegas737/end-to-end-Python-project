36e8c1cc43766b84e50657e8fc0f2c0589d137f3
Vegas737 <vegasgit737@gmail.com>
2026-05-03 20:18:19 +0400

    Update load.py

--- src/sem2_de/load.py
+++ src/sem2_de/load.py
@@ -2,14 +2,13 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 
-conn_url = "postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres"
+conn_url = "postgresql+psycopg2://student:student_pw@localhost:5433/analytics"
 engine = create_engine(conn_url)
 
-normalized_file = "data/normalized/holidays_cleaned.csv"
-
+normalized_file = "data/normalized/variant_20/2026-03-19_11-39-15_holidays_germany.csv"
 if os.path.exists(normalized_file):
     df = pd.read_csv(normalized_file)
-    print(f"✅ Загружаем данные из слоя Normalized: {normalized_file}")
+    print(f"Загружаем данные из слоя Normalized: {normalized_file}")
     
     with engine.begin() as connection:
         df.to_sql(
@@ -18,7 +17,9 @@ if os.path.exists(normalized_file):
             if_exists='replace',
             index=False
         )
-    print(f"✅ Успешно загружено {len(df)} строк в таблицу 'mart_holidays_germany'")
+    print(f"Успешно загружено {len(df)} строк в таблицу 'mart_holidays_germany'")
 else:
-    print(f"❌ Ошибка: Файл {normalized_file} не найден. Проверь этап Transform!")
+    import os
+    print(f"Ошибка: Файл не найден по пути: {os.path.abspath(normalized_file)}")
     exit(1)
+
