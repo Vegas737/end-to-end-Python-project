51a6cf523b752296b476314a896e05cfd91c73f8
Vegas737 <vegasgit737@gmail.com>
2026-05-17 18:13:08 +0400

    Update load.py

--- src/sem2_de/load.py
+++ src/sem2_de/load.py
@@ -2,10 +2,11 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 
-conn_url = "postgresql+psycopg2://student:student_pw@localhost:5433/analytics"
+conn_url = "postgresql+psycopg2://student:student_pw@postgres:5432/analytics"
 engine = create_engine(conn_url)
 
-normalized_file = "data/normalized/variant_20/2026-03-19_11-39-15_holidays_germany.csv"
+normalized_file = "/opt/airflow/data/normalized/holidays_cleaned.csv"
+
 if os.path.exists(normalized_file):
     df = pd.read_csv(normalized_file)
     print(f"Загружаем данные из слоя Normalized: {normalized_file}")
@@ -19,7 +20,5 @@ if os.path.exists(normalized_file):
         )
     print(f"Успешно загружено {len(df)} строк в таблицу 'mart_holidays_germany'")
 else:
-    import os
     print(f"Ошибка: Файл не найден по пути: {os.path.abspath(normalized_file)}")
     exit(1)
-
