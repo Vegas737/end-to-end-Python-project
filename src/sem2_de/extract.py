7fb0c4995ab55ffc731c56c92968d4d9194fafc8
Vegas737 <vegasgit737@gmail.com>
2026-05-25 10:34:32 +0400

    Update extract.py

--- src/sem2_de/extract.py
+++ src/sem2_de/extract.py
@@ -3,20 +3,21 @@ import yaml
 import requests
 import os
 import json
-from datetime import datetime
 
 def load_config(config_path):
     with open(config_path, 'r', encoding='utf-8') as f:
         return yaml.safe_load(f)
 
-def run_extract(config_path):
+def run_extract(config_path, run_date):
     config = load_config(config_path)
     
     base_url = config['api']['base_url']
     country = config['entity']['country_code']
-    year = config['entity']['year']
+    
+    year = run_date.split('-')[0]
     url = f"{base_url}/{year}/{country}"
     
+    print(f"--- Этап Extract: Запрос данных за {run_date} ---")
     print(f"Начинаю выгрузку данных из API: {url}")
     
     try:
@@ -27,22 +28,22 @@ def run_extract(config_path):
         out_dir = "/opt/airflow/data/raw"
         os.makedirs(out_dir, exist_ok=True)
         
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M")
-        file_path = os.path.join(out_dir, f"holidays_{country}_{year}_{timestamp}.json")
+        file_path = os.path.join(out_dir, f"raw_{run_date}.json")
         
         with open(file_path, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
             
-        print(f"Успех! Данные сохранены в: {file_path}")
+        print(f"✅ Успех! Данные сохранены в: {file_path}")
         print(f"Получено записей: {len(data)}")
 
     except Exception as e:
-        print(f"Произошла ошибка при выгрузке: {e}")
+        print(f"❌ Произошла ошибка при выгрузке: {e}")
+        raise e 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, required=True)
+    parser.add_argument("--date", type=str, required=True) 
     args = parser.parse_args()
     
-
-    run_extract(args.config)
+    run_extract(args.config, args.date)
