f9ca76a2feef404dd858897af7488fe6735d8bb4
Vegas737 <vegasgit737@gmail.com>
2026-05-25 10:35:11 +0400

    Update load.py

--- src/sem2_de/load.py
+++ src/sem2_de/load.py
@@ -1,24 +1,54 @@
+import argparse
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import os
 
-conn_url = "postgresql+psycopg2://student:student_pw@postgres:5432/analytics"
-engine = create_engine(conn_url)
+def run_load(run_date):
+    print(f"--- Этап Load: Загрузка периода {run_date} в БД ---")
+    
+    normalized_file = f"/opt/airflow/data/normalized/normalized_{run_date}.csv"
 
-normalized_file = "/opt/airflow/data/normalized/holidays_cleaned.csv"
-
-if os.path.exists(normalized_file):
+    if not os.path.exists(normalized_file):
+        print(f"❌ Ошибка: Файл не найден: {normalized_file}")
+        exit(1)
+        
     df = pd.read_csv(normalized_file)
     print(f"Загружаем данные из слоя Normalized: {normalized_file}")
     
+    target_year = int(run_date.split('-')[0])
+    
+    df['load_period_year'] = target_year
+
+    conn_url = "postgresql+psycopg2://student:student_pw@postgres:5432/analytics"
+    engine = create_engine(conn_url)
+    
     with engine.begin() as connection:
+        table_exists_query = text("""
+            SELECT EXISTS (
+                SELECT FROM information_schema.tables 
+                WHERE table_name = 'mart_holidays_germany'
+            );
+        """)
+        table_exists = connection.execute(table_exists_query).scalar()
+        
+        if table_exists:
+            delete_query = text("DELETE FROM mart_holidays_germany WHERE load_period_year = :year")
+            connection.execute(delete_query, {"year": target_year})
+            print(f"🗑 Очищены старые данные в БД за {target_year} год.")
+        else:
+            print("🆕 Таблицы ещё не существует. Она будет создана автоматически.")
+
         df.to_sql(
             name='mart_holidays_germany', 
             con=connection, 
-            if_exists='replace',
+            if_exists='append',
             index=False
         )
-    print(f"Успешно загружено {len(df)} строк в таблицу 'mart_holidays_germany'")
-else:
-    print(f"Ошибка: Файл не найден по пути: {os.path.abspath(normalized_file)}")
-    exit(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True)
+    parser.add_argument("--date", type=str, required=True) 
+    args = parser.parse_args()
+
+    run_load(args.date)
