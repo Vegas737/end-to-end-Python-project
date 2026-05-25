63ddf4b8ab0cbfd843290bbf778abd917b45ff10
Vegas737 <vegasgit737@gmail.com>
2026-05-25 10:35:25 +0400

    Update transform.py

--- src/sem2_de/transform.py
+++ src/sem2_de/transform.py
@@ -1,36 +1,46 @@
+import argparse
 import pandas as pd
 import json
 import os
-import glob
 
-def transform():
-    print("--- Этап Transform: Очистка данных ---")
+def transform(run_date):
+    print(f"--- Этап Transform: Очистка данных за {run_date} ---")
     
-    list_of_files = glob.glob('/opt/airflow/data/raw/*.json')
-    if not list_of_files:
-        print("❌ Нет данных для трансформации")
+    input_file = f'/opt/airflow/data/raw/raw_{run_date}.json'
+    
+    if not os.path.exists(input_file):
+        print(f"❌ Файл {input_file} не найден. Проверь этап Extract!")
         return False
-    
-    latest_file = max(list_of_files, key=os.path.getctime)
-    print(f"Обработка файла: {latest_file}")
+        
+    print(f"Обработка файла: {input_file}")
 
-    with open(latest_file, 'r', encoding='utf-8') as f:
+    with open(input_file, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    df = pd.DataFrame(data)
+    if not data:
+        print("⚠ Получен пустой массив данных от API")
+        df = pd.DataFrame()
+    else:
+        df = pd.DataFrame(data)
     
-    if 'date' in df.columns:
+    if 'date' in df.columns and not df.empty:
         df['date'] = pd.to_datetime(df['date'])
     
     os.makedirs('/opt/airflow/data/normalized', exist_ok=True)
-    out_path = '/opt/airflow/data/normalized/holidays_cleaned.csv'
+    
+    out_path = f'/opt/airflow/data/normalized/normalized_{run_date}.csv'
     df.to_csv(out_path, index=False)
     
     print(f"✅ Данные нормализованы и сохранены в {out_path}")
     return True
 
 if __name__ == "__main__":
-    if transform():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True)
+    parser.add_argument("--date", type=str, required=True)  
+    args = parser.parse_args()
+
+    if transform(args.date):
         exit(0)
     else:
         exit(1)
